598df4c6890534777d3f1e648ce6732261d5542d
Sanya <sasarybalko2809@gmail.ru>
2023-02-01 18:36:01 +0300

    upgrade static_maps_api

--- static_maps_api.py
+++ static_maps_api.py
@@ -1,19 +1,18 @@
 import requests
 
 
-def get_map(ll: tuple[float, float], spn: tuple[float, float], ) -> str:
-    response = requests.get("http://geocode-maps.yandex.ru/1.x/", params={
-        "ll": ','.join(map(str, ll)),
-        "spn": ','.join(map(str, spn)),
-        "l": spn,
+def get_map(*, ll: tuple[float, float], spn: tuple[float, float], map_type: str) -> str:
+    response = requests.get('http://static-maps.yandex.ru/1.x/', params={
+        'll': ','.join(map(str, ll)),
+        'spn': ','.join(map(str, spn)),
+        'l': map_type,
     })
 
     if not response:
         raise RuntimeError(
             f'''Ошибка выполнения запроса:
-            {response.request}
-            Http статус {response.status_code} ({response.reason})''')
-
+            {response.request.url}
+            Http статус: {response.status_code} ({response.reason})''')
 
     filename = 'map.png'
     with open(filename, 'wb') as file:
