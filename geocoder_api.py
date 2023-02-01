f47a4928e99bcae8e791a9804e9c95ea0345a258
Sanya <sasarybalko2809@gmail.ru>
2023-02-01 18:32:07 +0300

    upgrade geocoder_api

--- geocoder_api.py
+++ geocoder_api.py
@@ -1,5 +1,7 @@
 import requests
-def get_coordinates(address: str) -> tuple[float, float]:
+
+
+def get_coordinates(address: str) -> tuple[float | None, float | None]:
     toponym = geocoder_request(address)
     if toponym is None:
         return None, None
@@ -11,9 +13,32 @@ def get_coordinates(address: str) -> tuple[float, float]:
     return toponym_longitude, toponym_lattitude
 
 
-API_KEY = "40d1649f-0493-4b70-98ba-98533de7710b"
+def get_coordinates_and_span(address: str) -> tuple[tuple[float | None, float | None], tuple[float | None, float | None]]:
+    toponym = geocoder_request(address)
+    if toponym is None:
+        return (None, None), (None, None)
 
-def geocoder_request(address: str) -> dict:
+    # Координаты центра топонима:
+    toponym_coodrinates = toponym['Point']['pos']
+    # Широта, преобразованная в плавающее число:
+    toponym_longitude, toponym_lattitude = map(float, toponym_coodrinates.split(' '))
+
+    # Рамка вокруг объекта:
+    envelope = toponym['boundedBy']['Envelope']
+    # левая, нижняя, правая и верхняя границы из координат углов:
+    l, b = map(float, envelope['lowerCorner'].split())
+    r, t = map(float, envelope['upperCorner'].split())
+    # Вычисляем полуразмеры по вертикали и горизонтали
+    dx = abs(l - r) / 2
+    dy = abs(t - b) / 2
+
+    return (toponym_longitude, toponym_lattitude), (dx, dy)
+
+
+API_KEY = '40d1649f-0493-4b70-98ba-98533de7710b'
+
+
+def geocoder_request(address: str) -> dict | None:
     # Выполняем запрос.
     response = requests.get('http://geocode-maps.yandex.ru/1.x/', params={
         'apikey': API_KEY,
@@ -24,8 +49,8 @@ def geocoder_request(address: str) -> dict:
     if not response:
         raise RuntimeError(
             f'''Ошибка выполнения запроса:
-                {response.request.url}
-                Http статус: {response.status_code} ({response.reason})''')
+            {response.request.url}
+            Http статус: {response.status_code} ({response.reason})''')
 
     # Преобразуем ответ в json-объект
     data = response.json()
