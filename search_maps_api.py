0d6be3bb35dcd7ba8b2ff1b26a3585bbd3a89993
Sanya <sasarybalko2809@gmail.ru>
2023-02-04 15:45:57 +0300

    upgrade search_maps_api and static_maps_api

--- search_maps_api.py
+++ search_maps_api.py
@@ -4,7 +4,7 @@ from config import SEARCH_MAPS_API_KEY
 
 
 def find_nearest_organization(ll: tuple[float, float], span: tuple[float, float],
-                              organization_type: str) -> dict | None:
+                              organization_type: str):
     # Выполняем запрос.
     response = requests.get('http://search-maps.yandex.ru/1.x/', params={
         'apikey': SEARCH_MAPS_API_KEY,
