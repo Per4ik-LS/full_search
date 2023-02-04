d3d0b94f06c390ccc50bbebad36033d03d3a6e67
Sanya <sasarybalko2809@gmail.ru>
2023-02-04 14:12:29 +0300

    create show_map and main_full_search

--- show_map.py
+++ show_map.py
@@ -1,31 +1,7 @@
 import os
-from sys import argv
 import pygame
 
-from geocoder_api import (
-    get_coordinates,
-    get_coordinates_and_span,
-)
 from static_maps_api import get_map
-
-
-def main() -> None:
-    toponym_to_find = ' '.join(argv[1:])
-
-    if toponym_to_find == '':
-        print('No data')
-        return
-
-    # Показываем карту с фиксированным масштабом.
-    lat, lon = get_coordinates(toponym_to_find)
-    show_map(ll=(lat, lon), spn=(0.005, 0.005), map_type='map')
-
-    # Показываем карту с масштабом, подобранным по заданному объекту.
-    (lat, lon), (dx, dy) = get_coordinates_and_span(toponym_to_find)
-    show_map(ll=(lat, lon), spn=(dx, dy), map_type='map')
-
-    # Добавляем исходную точку на карту.
-    show_map(ll=(lat, lon), spn=(dx, dy), map_type='map', pt=(lat, lon))
 
 
 def show_map(*, ll: tuple[float, float], spn: tuple[float, float], map_type: str,
@@ -43,6 +19,3 @@ def show_map(*, ll: tuple[float, float], spn: tuple[float, float], map_type: str
     pygame.quit()
     # Удаляем за собой файл с изображением.
     os.remove(map_filename)
-
-
-main()
