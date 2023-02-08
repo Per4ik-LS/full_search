c282fdf17b06abb62a7b8af09782f2c17622ffcd
Sanya <sasarybalko2809@gmail.ru>
2023-02-08 17:08:21 +0300

    upgrade window.py

--- window.py
+++ window.py
@@ -13,7 +13,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self._map_zoom = 5
-        self._map_ll = 37.977751, 55.757718
+        self._map_ll = [37.977751, 55.757718]
         self._map_l = 'map'
         self._init_ui()
 
@@ -21,14 +21,25 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self._refresh_map()
 
     def keyPressEvent(self, event: QKeyEvent) -> None:
+        move_distance = 35 / self._map_zoom ** 2
         previous_zoom = self._map_zoom
+        previous_ll = self._map_ll.copy()
         if event.key() == Qt.Key_PageUp:
             self._map_zoom += 1
         elif event.key() == Qt.Key_PageDown:
             self._map_zoom -= 1
+        elif event.key() == Qt.Key_W or event.key() == Qt.Key_Up:
+            self._map_ll[1] += move_distance
+        elif event.key() == Qt.Key_A or event.key() == Qt.Key_Left:
+            self._map_ll[0] -= move_distance
+        elif event.key() == Qt.Key_D or event.key() == Qt.Key_Right:
+            self._map_ll[0] += move_distance
+        elif event.key() == Qt.Key_S or event.key() == Qt.Key_Down:
+            self._map_ll[1] -= move_distance
         self._map_zoom = self._clip_zoom(self._map_zoom, 1, 16)
-        if previous_zoom != self._map_zoom:
-            self._refresh_map()
+        if previous_zoom == self._map_zoom and previous_ll == self._map_ll:
+            return
+        self._refresh_map()
 
     def _clip_zoom(self, zoom: int, min_zoom: int, max_zoom: int) -> int:
         if zoom < min_zoom:
